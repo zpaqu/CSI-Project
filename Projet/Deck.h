c6a6bfb429385807721207d45ee878c3b71eaa07
Nidaig <ndaig049@uottawa.ca>
2015-12-09 22:23:46 -0500

    IS WORKING

--- Projet/Deck.h
+++ Projet/Deck.h
@@ -4,9 +4,7 @@
 #include <memory>
 #include "AnimalCard.h"
 
-<<<<<<< HEAD
 //using namespace std;
-=======
 //Auteurs: Nicolas Daigle (7223444), Zachary Paquette (7230016)
 //Cours: CSI2772
 //Date de remise: 9 Decembre 2015
@@ -23,4 +21,26 @@ public:
 private:
 
 };
->>>>>>> origin/master
+#include "Deck.h"
+
+
+//Auteurs: Nicolas Daigle (7223444), Zachary Paquette (7230016)
+//Cours: CSI2772
+//Date de remise: 9 Decembre 2015
+
+
+//Retourne le dernier element dans le Deck. Dans ce cas ce sera un objet de type AnimalCard
+template <class T>
+T Deck<T>::draw()
+{
+	T temp = back();
+	pop_back();
+
+	return temp;
+}
+
+template<class T>
+void Deck<T>::add(T t) // Ajoute un objet dans le Deck. Dans ce cas ce sera un objet de type AnimalCard
+{
+	push_back(t);
+}
